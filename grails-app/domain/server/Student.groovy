bc6fcccb3c1be1a7706f101bd955105efd08c634
z750mm13 <maurir_@hotmail.com>
2022-02-02 05:22:09 -0600

    Fin de la configuracion del servidor

---
+++ grails-app/domain/server/Student.groovy
@@ -0,0 +1,32 @@
+package server
+
+class Student {
+    int id;
+    String name;
+    String lastName;
+    String email;
+    int semester;
+    String enrollment;
+    String specialty;
+
+    static constraints = {
+        id(unique:true, nullable:false, blank:false)
+        name(nullable:false, blank:false)
+        lastname(nullable:false, blank:false)
+        email(unique:true, nullable:false, blank:false,email:true)
+        semester(nullable:false, blank:false)
+        enrollment inList: ['Enfermeria','Maestrias','Doctorados']
+        specialty inList: ['Enfermeria','Software','Arquitectura','Fiscal','Educacion','Comunicacion','Gastronomia']
+    }
+
+    static mapping = {
+        table 'students'
+        id column: 'id'
+        name column:'name'
+        lastname column:  'lastname'
+        email column: 'email'
+        semester column: 'semester'
+        enrollment column: 'enrollment'
+        specialty column: 'specialty'
+    }
+}
